964d8f7917fd853a0f8dac355952f7ac32446638
Andrew Hearts <andreyko.era@gmail.com>
2023-03-09 22:50:37 +0200

    fixing some issues with 'to string' method

--- Zoo/src/ua/lviv/iot/algo/part1/lab1/Zoo.java
+++ Zoo/src/ua/lviv/iot/algo/part1/lab1/Zoo.java
@@ -17,17 +17,17 @@ public class Zoo {
     private String location;
     private double area;
     private int capacity;
-    private static Zoo instance;
+    private static ua.lviv.iot.algo.part1.lab1.Zoo instance;
 
-    public static Zoo getInstance() {
+    public static ua.lviv.iot.algo.part1.lab1.Zoo getInstance() {
         if (instance == null) {
-            return new Zoo();
+            return new ua.lviv.iot.algo.part1.lab1.Zoo();
         }
         return instance;
     }
     @Override
     public String toString() {
-        return ("This %s zoo has capacity %d animals, has area %d km^2, also it locates in %s", name, capacity,area,location);
+        return ("This "+ name + "zoo has capacity " + capacity + " animals, has area " + area + " km^2, also it locates in " + location);
     }
     public void increaseCapacity(int number) {
         capacity += number;
@@ -41,14 +41,14 @@ public class Zoo {
 
     }
     public static void main(String[] args) {
-        Zoo[] zoos;
-        zoos = new Zoo[4];
-        zoos[0] = new Zoo();
+        ua.lviv.iot.algo.part1.lab1.Zoo[] zoos;
+        zoos = new ua.lviv.iot.algo.part1.lab1.Zoo[4];
+        zoos[0] = new ua.lviv.iot.algo.part1.lab1.Zoo();
         zoos[0].setArea(200.5);
         zoos[0].setName("Hoshin");
         zoos[0].setCapacity(1000);
         zoos[0].setLocation("Tokyo");
-        zoos[1] = new Zoo("Limpopo", "Lviv", 100.5, 600);
+        zoos[1] = new ua.lviv.iot.algo.part1.lab1.Zoo("Limpopo", "Lviv", 100.5, 600);
         zoos[2] = getInstance();
         zoos[3] = getInstance();
         int i = 0;
@@ -57,5 +57,3 @@ public class Zoo {
             i++;
         }
     }
-}
-
