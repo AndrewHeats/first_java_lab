0b5320fb7b18aa7d509f69ebf3bb7d1a570676a2
Andrew Hearts <andreyko.era@gmail.com>
2023-03-13 16:42:51 +0200

    adding fixed first lab file

--- Zoo/src/ua/lviv/iot/algo/part1/lab1/Zoo.java
+++ Zoo/src/ua/lviv/iot/algo/part1/lab1/Zoo.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -17,18 +16,12 @@ public class Zoo {
     private String location;
     private double area;
     private int capacity;
-    private static ua.lviv.iot.algo.part1.lab1.Zoo instance;
+    private static Zoo instance;
 
-    public static ua.lviv.iot.algo.part1.lab1.Zoo getInstance() {
-        if (instance == null) {
-            return new ua.lviv.iot.algo.part1.lab1.Zoo();
-        }
+    public static Zoo getInstance() {
         return instance;
     }
-    @Override
-    public String toString() {
-        return ("This "+ name + "zoo has capacity " + capacity + " animals, has area " + area + " km^2, also it locates in " + location);
-    }
+
     public void increaseCapacity(int number) {
         capacity += number;
     }
@@ -38,22 +31,17 @@ public class Zoo {
     public void addNewRegion(double area) {
         location += " and New Region";
         this.area += area;
-
     }
     public static void main(String[] args) {
-        ua.lviv.iot.algo.part1.lab1.Zoo[] zoos;
-        zoos = new ua.lviv.iot.algo.part1.lab1.Zoo[4];
-        zoos[0] = new ua.lviv.iot.algo.part1.lab1.Zoo();
-        zoos[0].setArea(200.5);
-        zoos[0].setName("Hoshin");
-        zoos[0].setCapacity(1000);
-        zoos[0].setLocation("Tokyo");
-        zoos[1] = new ua.lviv.iot.algo.part1.lab1.Zoo("Limpopo", "Lviv", 100.5, 600);
-        zoos[2] = getInstance();
-        zoos[3] = getInstance();
-        int i = 0;
-        while (i < zoos.length) {
-            System.out.println(zoos[i]);
-            i++;
+        Zoo[] zoos = {
+                new Zoo(),
+                new Zoo("Limpopo", "Lviv", 100.5, 600),
+                Zoo.getInstance(),
+                Zoo.getInstance()
+        };
+
+        for(Zoo zoo: zoos){
+            System.out.println(zoo);
         }
     }
+}
